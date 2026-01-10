ccc78816a1c53a7bd057a88bb53d7cd4757fc4a8
Dima011099 <uengein4@gmail.com>
2026-01-10 23:23:54 +0300

    add new table project in bd

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -17,17 +17,30 @@ class DatabaseHelper {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    return await openDatabase(path, version: 1, onCreate: _createDB, onOpen: (db) async {await db.execute('PRAGMA foreign_keys = ON');});
   }
 
   Future _createDB(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        status INTEGER NOT NULL -- 0: Задачи, 1: В работе, 2: Результат
+      CREATE TABLE projects (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL
       )
     ''');
+
+    await db.execute('''
+        CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        project_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        status INTEGER NOT NULL, -- 0: Задачи, 1: В работе, 2: Результат
+        difficulty INTEGER NOT NULL DEFAULT 1, -- Сложность (1-3)
+        created_at TEXT NOT NULL,            -- Дата создания
+        FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
+        )
+    ''');
+
+    createProject("Base Project");
   }
 
   Future<List<Map<String, dynamic>>> readAllTasks() async {
@@ -35,9 +48,22 @@ class DatabaseHelper {
     return await db.query('tasks');
   }
 
+   Future<int> createProject (String title) async {
+     final db = await instance.database;
+     return await db.insert('projects', {'name': title});
+   }
+
   Future<int> createTask(String title) async {
     final db = await instance.database;
-    return await db.insert('tasks', {'title': title, 'status': 0});
+  //  return await db.insert('tasks', {'title': title, 'status': 0});
+
+    return await db.insert('tasks', {
+      'title': title,
+      'status': 0,
+      'difficulty': 1,
+      'project_id': 1,
+      'created_at': DateTime.now().toIso8601String() // Формат: 2026-01-10T22:30...
+    });
   }
 
   Future<int> updateTaskStatus(int id, int status) async {
